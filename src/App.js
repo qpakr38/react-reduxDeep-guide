34a7300c153bd16cb5314e14a7d6cdadcb5b5e4f
qpakr38 <zczc0819@gmail.com>
2022-09-12 19:25:51 +0900

    FEAT : 전송상태 Redux로 관리하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {useSelector} from "react-redux";
-import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {Fragment, useEffect} from "react";
+import {uiAction} from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
+    const dispatch = useDispatch();
     const showCart = useSelector(state => state.ui.cartIsVisible);
     const cart = useSelector(state => state.cart);
+    const notification = useSelector(state => state.ui.notification);
     useEffect(() => {
-        fetch("https://react-http-f752e-default-rtdb.firebaseio.com/cart.json", {
-            method: 'PUT',
-            body: JSON.stringify(cart),
-        });
-    }, [cart]);
+        const sendCartData = async () => {
+            dispatch(uiAction.showNotification({
+                status: 'pending',
+                title: 'Sending...',
+                message: 'Sending cart data!'
+            }));
+            const response = await fetch(
+                "https://react-http-f752e-default-rtdb.firebaseio.com/cart.json",
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(cart),
+                });
+            if (!response.ok) {
+                throw new Error('Sending cart data failed!');
+            }
+            dispatch(uiAction.showNotification({
+                status: 'success',
+                title: 'Success',
+                message: 'Sent cart data successfully!'
+            }));
+        };
+        if(isInitial){
+            isInitial = false;
+            return;
+        }
+        sendCartData().catch(error => {
+            dispatch(uiAction.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Sending cart data failed!'
+            }));
+        })
+    }, [cart, dispatch]);
     return (
-        <Layout>
-            {showCart && <Cart/>}
-            <Products/>
-        </Layout>
+        <Fragment>
+            {notification && (
+                <Notification
+                    status = {notification.status}
+                    title = {notification.title}
+                    message = {notification.message}
+                />)}
+            <Layout>
+                {showCart && <Cart/>}
+                <Products/>
+            </Layout>
+
+        </Fragment>
     );
 }
 
