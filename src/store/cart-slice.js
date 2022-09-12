338b12bdc7d43860768bbf8591332b8999b399f9
qpakr38 <zczc0819@gmail.com>
2022-09-12 17:53:52 +0900

    FEAT: Cart Slice 생성

---
+++ src/store/cart-slice.js
@@ -0,0 +1,45 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+        totalQuantity: 0,
+        totalAmount: 0
+    },
+    reducers: {
+        addItemToCart(state, action) {
+            state.totalQuantity++;
+            const addItem = action.payload;
+            const existingItem = state.items.find(item => item.id === addItem.id);
+            if (!existingItem) {
+                state.items.push({
+                    id: addItem.id,
+                    price: addItem.price,
+                    quantity: 1,
+                    totalPrice: addItem.price,
+                    name: addItem.title
+                });
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + addItem.price;
+            }
+        },
+        removeItemFromCart(state, action) {
+            state.totalQuantity--;
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        }
+    }
+});
+
+export const cartAction = cartSlice.actions;
+export const cartReduce = cartSlice.reducer;
+
+export default cartSlice;
