bc57e1c70dbcc8869ab126db57b71021ffdeab28
qpakr38 <zczc0819@gmail.com>
2022-09-12 18:12:00 +0900

    FEAT : Slice를 활용한 카트 상품수 출력

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,16 +1,17 @@
 import classes from './CartButton.module.css';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {uiAction} from "../../store/ui-slice";
 
 const CartButton = (props) => {
     const dispatch =useDispatch();
+    const totalQuantity = useSelector(state=>state.cart.totalQuantity);
     const toggleCartHandler=()=>{
         dispatch(uiAction.toggle());
     };
   return (
     <button onClick={toggleCartHandler} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalQuantity}</span>
     </button>
   );
 };
