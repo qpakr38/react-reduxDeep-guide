71b4675dd9ad1b295b6d1c1be879fbb0d9ea9fb9
qpakr38 <zczc0819@gmail.com>
2022-09-12 16:18:10 +0900

    FEAT: Reducer 연결

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
 import classes from './CartButton.module.css';
+import {useDispatch} from "react-redux";
+import {uiAction} from "../../store/ui-slice";
 
 const CartButton = (props) => {
+    const dispatch =useDispatch();
+    const toggleCartHandler=()=>{
+        dispatch(uiAction.toggle());
+    };
   return (
-    <button className={classes.button}>
+    <button onClick={toggleCartHandler} className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
