5a974553366ecc6c11d6600d1dd1d6e516a43897
qpakr38 <zczc0819@gmail.com>
2022-09-12 22:08:34 +0900

    REFACTORY : action 따로 관리하기

---
+++ src/store/cart-actions.js
@@ -0,0 +1,38 @@
+import {uiAction} from "./ui-slice";
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(uiAction.showNotification({
+            status: 'pending',
+            title: 'Sending...',
+            message: 'Sending cart data!'
+        }));
+        const sendRequest = async () => {
+            const response = await fetch(
+                "https://react-http-f752e-default-rtdb.firebaseio.com/cart.json",
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(cart),
+                });
+            if (!response.ok) {
+                throw new Error('Sending cart data failed!');
+            }
+        }
+        try {
+            await sendRequest();
+            dispatch(uiAction.showNotification({
+                status: 'success',
+                title: 'Success',
+                message: 'Sent cart data successfully!'
+            }));
+        } catch (error) {
+            sendCartData().catch(error => {
+                dispatch(uiAction.showNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: 'Sending cart data failed!'
+                }));
+            });
+        }
+    };
+};
