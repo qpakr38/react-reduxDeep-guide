069d991eb1afce5a774ae56c80ddd042c4abd45a
qpakr38 <zczc0819@gmail.com>
2022-09-12 22:06:14 +0900

    FEAT : Thunk와 firebase를 통한 데이터베이스에 저장하기

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useDispatch, useSelector} from "react-redux";
 import {Fragment, useEffect} from "react";
-import {uiAction} from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import {sendCartData} from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -14,38 +14,11 @@ function App() {
     const cart = useSelector(state => state.cart);
     const notification = useSelector(state => state.ui.notification);
     useEffect(() => {
-        const sendCartData = async () => {
-            dispatch(uiAction.showNotification({
-                status: 'pending',
-                title: 'Sending...',
-                message: 'Sending cart data!'
-            }));
-            const response = await fetch(
-                "https://react-http-f752e-default-rtdb.firebaseio.com/cart.json",
-                {
-                    method: 'PUT',
-                    body: JSON.stringify(cart),
-                });
-            if (!response.ok) {
-                throw new Error('Sending cart data failed!');
-            }
-            dispatch(uiAction.showNotification({
-                status: 'success',
-                title: 'Success',
-                message: 'Sent cart data successfully!'
-            }));
-        };
         if(isInitial){
-            isInitial = false;
+            isInitial =false;
             return;
         }
-        sendCartData().catch(error => {
-            dispatch(uiAction.showNotification({
-                status: 'error',
-                title: 'Error!',
-                message: 'Sending cart data failed!'
-            }));
-        })
+        dispatch(sendCartData(cart));
     }, [cart, dispatch]);
     return (
         <Fragment>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
+import {uiAction} from "./ui-slice";
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -38,6 +39,42 @@ const cartSlice = createSlice({
         }
     }
 });
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(uiAction.showNotification({
+            status: 'pending',
+            title: 'Sending...',
+            message: 'Sending cart data!'
+        }));
+        const sendRequest = async () => {
+            const response = await fetch(
+                "https://react-http-f752e-default-rtdb.firebaseio.com/cart.json",
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(cart),
+                });
+            if (!response.ok) {
+                throw new Error('Sending cart data failed!');
+            }
+        }
+        try {
+            await sendRequest();
+            dispatch(uiAction.showNotification({
+                status: 'success',
+                title: 'Success',
+                message: 'Sent cart data successfully!'
+            }));
+        } catch (error) {
+            sendCartData().catch(error => {
+                dispatch(uiAction.showNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: 'Sending cart data failed!'
+                }));
+            });
+        }
+    };
+};
 
 export const cartAction = cartSlice.actions;
 export const cartReduce = cartSlice.reducer;
